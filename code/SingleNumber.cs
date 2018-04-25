53fc0d73e7b027b2e79c240aed5e3568fefa26d6
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-04-25 00:05:08 -0700

    SingleNumber

--- code/SingleNumber.cs
+++ code/SingleNumber.cs
@@ -17,16 +17,63 @@ namespace code
         public void SingleNumber()
         {
             List<int> nums = new List<int>() { 4, 1, 2, 1, 2 };
-            SingleNumber(nums.ToArray());
+            int x = SingleNumber(nums.ToArray());
+            Console.WriteLine(x);
         }
 
         public int SingleNumber(int[] nums)
         {
-            PrintArray(nums);
+            // XOR the same number and get 0
+
+            for (int i=1; i<nums.Length; i++)
+            {
+                nums[0] ^= nums[i];
+            }
+
+            return nums[0];
+        }
+
+            public int SingleNumber_Naive(int[] nums)
+        {
+            //PrintArray(nums);
 
             // [4,1,2,1,2]
 
-            return 0;
+            int[,] tracking = new int[nums.Length, 2];
+
+            // go through the array and find each pair
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool foundMatch = false;
+                for (int j = 0; j < tracking.GetLength(0); j++)
+                {
+                    if (tracking[j, 1] == 1 && tracking[j, 0] == nums[i])
+                    {
+                        // found a match
+                        foundMatch = true;
+                        tracking[j, 1] = 2; // this means a pair was found
+                    }
+                }
+
+                if (!foundMatch)
+                {
+                    // add it to the tracking array
+                    tracking[i, 0] = nums[i];
+                    tracking[i, 1] = 1; // this means one instance of the int was found
+                }
+            }
+
+            // there should be only one unmatched int left
+            int unmatched = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(tracking[i,1] == 1)
+                {
+                    unmatched = tracking[i, 0];
+                }
+            }
+
+            return unmatched;
 
         }
 
