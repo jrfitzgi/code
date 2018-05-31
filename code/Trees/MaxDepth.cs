b71837d483cf93a3018211d5a41c6d1972c92746
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-05-30 21:56:16 -0700

    maxdepth

---
+++ code/Trees/MaxDepth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.Trees
+{
+    /*
+    Given a binary tree, find its maximum depth.
+    The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+    Note: A leaf is a node with no children.
+    https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
+    */
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int x) { val = x; }
+     * }
+     */
+    public partial class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) { return 0; }
+
+            int leftDepth = MaxDepth(root.left);
+            int rightDepth = MaxDepth(root.right);
+
+            return 1 + Math.Max(leftDepth, rightDepth);
+
+        }
+    }
+}
