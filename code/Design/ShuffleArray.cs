b4f842be5782730bbda3a3382a7f61a29c930f2d
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-06-05 22:22:16 -0700

    maxsubarray, shuffle array

---
+++ code/Design/ShuffleArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.Design
+{
+    // Shuffle a set of numbers without duplicates
+    // https://leetcode.com/problems/shuffle-an-array/description/
+
+    //public class Solution
+    //{
+
+    //    public Solution(int[] nums)
+    //    {
+    //        this.nums = nums;
+    //        r = new Random();
+    //    }
+
+    //    private int[] nums;
+    //    private Random r;
+
+    //    /** Resets the array to its original configuration and return it. */
+    //    public int[] Reset()
+    //    {
+    //        return this.nums;
+    //    }
+
+    //    /** Returns a random shuffling of the array. */
+    //    public int[] Shuffle()
+    //    {
+    //        if (this.nums == null || this.nums.Length <= 1) { return this.nums; }
+
+    //        int[] shuffled = new int[nums.Length];
+    //        for (int i = 0; i < nums.Length; i++)
+    //        {
+    //            shuffled[i] = nums[i];
+    //        }
+
+    //        for (int i = 0; i < nums.Length; i++)
+    //        {
+    //            int swapIndex = this.r.Next(i, nums.Length);
+    //            int temp = shuffled[i];
+    //            shuffled[i] = shuffled[swapIndex];
+    //            shuffled[swapIndex] = temp;
+    //        }
+
+    //        return shuffled;
+    //    }
+    //}
+
+    ///**
+    // * Your Solution object will be instantiated and called as such:
+    // * Solution obj = new Solution(nums);
+    // * int[] param_1 = obj.Reset();
+    // * int[] param_2 = obj.Shuffle();
+    // */
+}
---
+++ code/DynamicProgramming/MaxSubArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/566/
+
+    public partial class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            int max = dp[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+                max = Math.Max(dp[i], max);
+            }
+
+            return max;
+        }
+    }
+}
