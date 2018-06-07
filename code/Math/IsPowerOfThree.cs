69919d4d5323b3592525fec91f9139ec974d2929
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-06-07 00:45:04 -0700

    power of three

---
+++ code/Math/IsPowerOfThree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    // Given an integer, write a function to determine if it is a power of three.
+
+    // https://leetcode.com/submissions/detail/157794826/
+
+    public partial class Solution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+
+            int i = -1;
+            while (true)
+            {
+                i++;
+
+                if (n == Math.Pow(3, i))
+                {
+                    return true;
+                }
+
+
+                if (Math.Pow(3, i) > n || Math.Pow(3, i) <= 0)
+                {
+                    break;
+                }
+            }
+
+            return false;
+        }
+    }
+}
