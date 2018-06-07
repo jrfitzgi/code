5eb0345a52355be41977dc0134b6cb5f3e775573
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-06-06 22:05:38 -0700

    minstack, fizzbuzz

---
+++ code/Design/MinStack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.Design
+{
+
+    // Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+    // push(x) -- Push element x onto stack.
+    // pop() -- Removes the element on top of the stack.
+    // top() -- Get the top element.
+    // getMin() -- Retrieve the minimum element in the stack.
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/98/design/562/
+
+
+    //public class Node
+    //{
+    //    public Node() { }
+    //    public Node(int val, int min) { this.val = val; this.min = min; }
+    //    public int val;
+    //    public int min;
+    //    public Node next;
+
+    //}
+
+    //public class MinStack
+    //{
+
+    //    /** initialize your data structure here. */
+    //    public MinStack()
+    //    {
+
+    //    }
+
+    //    private Node top = null;
+
+    //    public void Push(int x)
+    //    {
+    //        if (top == null)
+    //        {
+    //            top = new Node(x, x);
+    //        }
+    //        else
+    //        {
+    //            int newMin = Math.Min(top.min, x);
+    //            Node newTop = new Node(x, newMin);
+    //            newTop.next = top;
+    //            top = newTop;
+    //        }
+    //    }
+
+    //    public void Pop()
+    //    {
+    //        if (top == null)
+    //        {
+    //            return;
+    //        }
+    //        else
+    //        {
+    //            top = top.next;
+    //        }
+    //    }
+
+
+    //    public int Top()
+    //    {
+    //        // in real life, if top == null then throw
+
+    //        return top.val;
+    //    }
+
+    //    public int GetMin()
+    //    {
+    //        // in real life, if size is 0 then throw exception
+    //        return top.min;
+    //    }
+    //}
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.Push(x);
+     * obj.Pop();
+     * int param_3 = obj.Top();
+     * int param_4 = obj.GetMin();
+     */
+}
---
+++ code/Math/FizzBuzz.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    // Write a program that outputs the string representation of numbers from 1 to n.
+    // But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/743/
+
+    public partial class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+
+            List<string> result = new List<string>();
+
+            if (n <= 0) { return result; }
+
+            int fizz = 3;
+            int fizzCounter = 0;
+            int buzz = 5;
+            int buzzCounter = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fizzCounter++;
+                buzzCounter++;
+
+                if (fizzCounter == fizz && buzzCounter == buzz)
+                {
+                    result.Add("FizzBuzz");
+                    fizzCounter = 0;
+                    buzzCounter = 0;
+                }
+                else if (fizzCounter == fizz)
+                {
+                    result.Add("Fizz");
+                    fizzCounter = 0;
+                }
+                else if (buzzCounter == buzz)
+                {
+                    result.Add("Buzz");
+                    buzzCounter = 0;
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+
+            return result;
+
+
+        }
+    }
+}
