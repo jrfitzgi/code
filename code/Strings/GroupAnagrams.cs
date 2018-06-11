213165d2e47d08fd822546d1f9aa19f7df85b824
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-06-11 00:33:16 -0700

    group anagrams

---
+++ code/Strings/GroupAnagrams.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.Strings
+{
+    // Given an array of strings, group anagrams together.
+    // https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/778/
+
+    public partial class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+            foreach (string str in strs)
+            {
+                string key = SortString(str);
+
+                if (dict.ContainsKey(key))
+                {
+                    dict[key].Add(str);
+                }
+                else
+                {
+                    dict.Add(key, new List<string> { str });
+                }
+            }
+
+            List<List<string>> result = new List<List<string>>();
+            foreach (string key in dict.Keys)
+            {
+                result.Add(dict[key]);
+            }
+
+            return result;
+
+
+        }
+
+        public static string SortString(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+    }
+}
