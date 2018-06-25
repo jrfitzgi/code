2f22b47e01b88d6b7a6bf56c45e94cb8bec38515
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-06-24 20:50:11 -0700

    GetIntersectionNode

---
+++ code/LinkedList/AddTwoNumbers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.LinkedList.AddTwoNumbers
+{
+    // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+    // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    // https://leetcode.com/explore/interview/card/top-interview-questions-medium/107/linked-list/783/
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int x) { val = x; }
+     * }
+     */
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode(0);
+            ListNode l3 = result;
+
+            while (true)
+            {
+                l3.next = new ListNode(0);
+                l3.val = SumNodes(l1, l2) + l3.val;
+                if (l3.val >= 10)
+                {
+                    l3.val = l3.val - 10;
+                    l3.next.val = 1;
+                }
+
+                if (l1 != null) { l1 = l1.next; }
+                if (l2 != null) { l2 = l2.next; }
+                if (l1 == null && l2 == null) // at the end of both lists
+                {
+                    if (l3.next.val == 0) // if we didn't carry over a 1, remove the last node which contains 0
+                    {
+                        l3.next = null;
+                    }
+                    break;
+                }
+
+                l3 = l3.next;
+
+            }
+
+            return result;
+        }
+
+        private int SumNodes(ListNode l1, ListNode l2)
+        {
+            if (l1 == null && l2 == null) { return 0; }
+            if (l1 == null) { return l2.val; }
+            if (l2 == null) { return l1.val; }
+
+            return l1.val + l2.val;
+        }
+    }
+}
---
+++ code/LinkedList/GetIntersectionNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.LinkedList.GetIntersectionNode
+{
+    //Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+    //For example, the following two linked lists:
+
+    //A:          a1 → a2
+    //                   ↘
+    //                     c1 → c2 → c3
+    //                   ↗            
+    //B:     b1 → b2 → b3
+    //begin to intersect at node c1.
+
+
+    //Notes:
+
+    //If the two linked lists have no intersection at all, return null.
+    //The linked lists must retain their original structure after the function returns.
+    //You may assume there are no cycles anywhere in the entire linked structure.
+    //Your code should preferably run in O(n) time and use only O(1) memory.
+
+    // https://leetcode.com/explore/interview/card/top-interview-questions-medium/107/linked-list/785/
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int x) { val = x; }
+     * }
+     */
+    public class Solution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+
+            if (headA == null || headB == null) { return null; }
+
+            int lenA = GetLength(headA);
+            int lenB = GetLength(headB);
+
+            ListNode s; // short
+            ListNode l; // long
+
+            if (lenA >= lenB)
+            {
+                l = headA;
+                s = headB;
+            }
+            else
+            {
+                l = headB;
+                s = headA;
+            }
+
+            for (int i = 0; i < Math.Abs(lenA - lenB); i++) // advance the long pointer to align with short pointer
+            {
+                l = l.next;
+            }
+
+            while (l != null) // could use either long or short here since they're aligned from the tail
+            {
+                if (l.val == s.val)
+                {
+                    return l;
+                }
+
+                l = l.next;
+                s = s.next;
+            }
+
+            return null; // no intersection
+
+
+        }
+
+        private int GetLength(ListNode n)
+        {
+            int length = 0;
+
+            while (n != null)
+            {
+                length++;
+                n = n.next;
+            }
+
+            return length;
+        }
+    }
+}
