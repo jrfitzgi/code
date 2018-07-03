797f72d09328fc1047aeb0d88a4e704142d91e78
Jordan Fitzgibbon <Jordanf@microsoft.com>
2018-07-03 00:04:24 -0700

    permutations

---
+++ code/Backtracking/Permutations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.Backtracking.Permutations
+{
+    // Given a collection of distinct integers, return all possible permutations.
+    // https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/795/
+
+    public class Solution
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+
+            List<IList<int>> results = new List<IList<int>>();
+
+            if (nums == null) { return results; }
+
+            HelpPermute(results, nums.ToList(), new List<int>());
+            return results;
+        }
+
+        private void HelpPermute(List<IList<int>> results, List<int> nums, List<int> cur)
+        {
+            if (nums.Count() == 0)
+            {
+                results.Add(cur);
+                return;
+            }
+
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                int num = nums[i];
+                List<int> newCur = cur.Concat(new List<int> { num }).ToList();
+                List<int> newNums = new List<int>(nums);
+                newNums.Remove(num);
+                HelpPermute(results, newNums, newCur);
+            }
+
+        }
+    }
+}
