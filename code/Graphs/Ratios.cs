231710e225768f1e5a9bf3775368717b9352067a
Jordan Fitzgibbon <Jordanf@microsoft.com>
2019-04-28 22:34:05 -0700

    ratios description

---
+++ code/Graphs/Ratios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code.Graphs
+{
+    /*
+    Ratios
+    Given a list of ratios, find other ratios
+
+    Eg.
+    cm:m=1/100
+    mm:m=1/1000
+    km:m=1000/1
+
+    Find cm:km
+     */
+
+    class Ratios
+    {
+    }
+}
